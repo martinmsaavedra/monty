73ebe424526e54a212dc22bfa7c2f33e8d5a2b66
martinmsaavedra <martin@bildung.uy>
2020-12-22 15:02:21 +0000

    task 14

--- bytecodes/36.m
+++ bytecodes/36.m
@@ -6,5 +6,3 @@ push 5
 pall
 rotr
 pall
-rotr
-pall
--- functions_2.c
+++ functions_2.c
@@ -102,22 +102,29 @@ void f_rotl(stack_s **stack, unsigned int line_number)
  **/
 void f_rotr(stack_s **stack, unsigned int line_number)
 {
-	stack_s *aux;
+	stack_s *aux, *new;
 	int stack_len = 0;
-	unsigned int temp, temp2;
 
 	(void)line_number;
 	stack_len = list_len(stack);
 	if (*stack != NULL && stack_len >= 2)
 	{
+		new = malloc(sizeof(stack_s));
+		if (!new)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			free_stack(*stack);
+			exit(EXIT_FAILURE);
+		}
+		new->n = (*stack)->n;
+		new->next = NULL;
 		aux = *stack;
-		temp = aux->n;
+		*stack = aux->next;
+		free(aux);
+		aux = *stack;
 		while (aux->next)
 			aux = aux->next;
-
-		temp2 = aux->n;
-		aux->n = temp;
-		aux = *stack;
-		aux->n = temp2;
+		aux->next = new;
+		new->prev = aux;
 	}
 }
